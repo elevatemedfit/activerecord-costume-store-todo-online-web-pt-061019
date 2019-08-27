183e2b3bb8d3b0390f99b1c9aeaff76c8ae402d0
Chris Junker <elevatemedfit@gmail.com>
2019-08-27 19:36:20 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -13,6 +13,7 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
       t.string :name
       t.integer :price
       t.string :size
+      t.string :image_url
       t.timestamps
     end
   end
